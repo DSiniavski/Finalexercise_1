6f3b196fae3bc5b21cd34a6d08ca64e18fca8ce1
DSiniavski <114159287+DSiniavski@users.noreply.github.com>
2022-11-26 16:52:43 +0300

    Добавлена программа

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿string[] array;
+Console.WriteLine("Enter elements separated by a space ");
+string userenter = Console.ReadLine()??"";
+array = userenter.Split(' ');
+            
+string[] sortedarray = ChooseShortString(array);
+
+PrintArray(array);
+PrintResult(sortedarray);
+
+string[] ChooseShortString(string[] array)
+  {
+       
+   string[] result = new string[array.Length];
+    int  newindex = 0;
+    int maxlenght = 3;
+    for (int i = 0; i < array.Length; i++ )
+    {
+      if (array[i].Length <= maxlenght)
+      {
+      result[newindex] = array[i];
+      newindex++;
+      }
+     }
+     Array.Resize(ref result, newindex);
+    return result;
+  }
+  
+
+  void PrintArray(string[] array)
+  {
+    Console.Write ($"[ ");
+      for(int i = 0; i < array.Length-1; i++) 
+      Console.Write ($"{array[i]}, ");
+        Console.Write ($"{array[array.Length-1]}] -> ");
+  }
+  
+  void PrintResult(string[] sortrdarray)
+  {
+    
+    if (sortedarray.Length  == 0)
+     {
+      Console.Write ($"[]");
+     }
+     else
+     {
+      Console.Write ($"[ ");
+      for (int j =0; j < sortedarray.Length-1; j++)
+
+      Console.Write($"{sortedarray[j]}, ");
+      Console.Write ($"{sortedarray[sortedarray.Length-1]}]");
+     }
+   
+  }
